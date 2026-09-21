2e7ebd40bb503a3a8b51d0d5b3d7209d9e598fd7
Joseph Mutiso <josephnzau206@gmail.com>
2026-09-21 19:06:00 +0300

    Create Fresh Cut Barbershop website

---
+++ business-website/script.js
@@ -0,0 +1,8 @@
+ console.log("Javascript is working");
+    const appointmentBtn =
+    document.getElementById("appointmentBtn");
+appointmentBtn.addEventListener(
+    "click",function() {
+        alert("Thanks for choosing Fresh Cut Barbershop");
+    }
+);
