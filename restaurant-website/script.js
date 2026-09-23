2f47e5fc49160333295359a7bae08525320be67e
Joseph Mutiso <josephnzau206@gmail.com>
2026-09-23 14:07:07 +0300

    Add restaurant website

---
+++ restaurant-website/script.js
@@ -0,0 +1,5 @@
+const contactButton = document.querySelector("#contact a");
+
+contactButton.addEventListener("click", function() {
+    alert("Thanks for contacting Savanna Bites! We look forward to serving you.");
+});
